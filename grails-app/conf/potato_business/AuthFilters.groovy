b9e897ae6ea368b1b1709fd4ffd6a0ffd9d5dc97
sunsam <123456>
2015-07-11 00:28:39 +0800

    add order

---
+++ grails-app/conf/potato_business/AuthFilters.groovy
@@ -0,0 +1,29 @@
+package potato_business
+
+import org.apache.commons.logging.LogFactory
+
+import java.text.MessageFormat
+
+class AuthFilters {
+
+    def cookieService
+    private static final log = LogFactory.getLog(this)
+
+    def filters = {
+
+        someUri(uri:'/web/**'){
+            before = {
+                log.error('start web interceptor')
+                if(!cookieService.getCookie("unionid")) {
+                    def redirectUrl = MessageFormat.format(grailsApplication.config.weixin.oauth2.url, [grailsApplication.config.wechat.config.appid,
+                                                                                                        URLEncoder.encode(grailsApplication.config.weixin.oauth2.redirectUri)].toArray())
+                    redirect(url: redirectUrl)
+                    return false
+                }
+                else {
+                    return true
+                }
+            }
+        }
+    }
+}
